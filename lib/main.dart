4ece6197a79cff619e0b9af507f0fb08d7080473
Nick-Joseph <njoseph321@gmail.com>
2023-01-30 19:24:10 -0500

    refactoring code and organizing

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+    return BlocProvider(
+      create: (context) => SubjectBloc(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: AuthPage(),
+        ),
     );
   }
 }
